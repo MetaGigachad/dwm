2fc9590c25d768055e74e43bd6208d40e5993bf0
metagigachad <288993@gmail.com>
2023-01-15 11:10:10 +0300

    Applied vanitygaps patch

--- config.def.h
+++ config.def.h
@@ -1,8 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 10;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -179,7 +184,24 @@ static Keychord keychords[] = {
 	{1, {{MODKEY|ShiftMask, XK_q}},				 quit,           {0} }, // Quit DWM
 	{1, {{MODKEY|ShiftMask, XK_r}},        quit,           {1} }, // Restart DWM 
 
-  /* volume keys */
+// Vanity gaps keys
+
+ 	{1, {{MODKEY|Mod4Mask,              XK_h}},      incrgaps,       {.i = +1 } },
+ 	{1, {{MODKEY|Mod4Mask,              XK_l}},      incrgaps,       {.i = -1 } },
+ 	{1, {{MODKEY|Mod4Mask|ShiftMask,    XK_h}},      incrogaps,      {.i = +1 } },
+ 	{1, {{MODKEY|Mod4Mask|ShiftMask,    XK_l}},      incrogaps,      {.i = -1 } },
+ 	{1, {{MODKEY|Mod4Mask|ControlMask,  XK_h}},      incrigaps,      {.i = +1 } },
+ 	{1, {{MODKEY|Mod4Mask|ControlMask,  XK_l}},      incrigaps,      {.i = -1 } },
+ 	{1, {{MODKEY|Mod4Mask,              XK_0}},      togglegaps,     {0} },
+ 	{1, {{MODKEY|Mod4Mask|ShiftMask,    XK_0}},      defaultgaps,    {0} },
+ 	{1, {{MODKEY,                       XK_y}},      incrihgaps,     {.i = +1 } },
+ 	{1, {{MODKEY,                       XK_o}},      incrihgaps,     {.i = -1 } },
+ 	{1, {{MODKEY|ControlMask,           XK_y}},      incrivgaps,     {.i = +1 } },
+ 	{1, {{MODKEY|ControlMask,           XK_o}},      incrivgaps,     {.i = -1 } },
+ 	{1, {{MODKEY|Mod4Mask,              XK_y}},      incrohgaps,     {.i = +1 } },
+ 	{1, {{MODKEY|Mod4Mask,              XK_o}},      incrohgaps,     {.i = -1 } },
+ 	{1, {{MODKEY|ShiftMask,             XK_y}},      incrovgaps,     {.i = +1 } },
+ 	{1, {{MODKEY|ShiftMask,             XK_o}},      incrovgaps,     {.i = -1 } },
 };
 
 /* button definitions */
